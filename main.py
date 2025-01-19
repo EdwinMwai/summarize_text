32247b85bb79aa4037f8442fac5865e6736fb6b4
Edwin Mwai <edwingitemi@gmail.com>
2025-01-19 19:17:47 +0300

    add data validation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from summarize_text.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from summarize_text.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from summarize_text.logging import logger
 
 
@@ -16,3 +17,17 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Data Validation"
+
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=======================x")
+
+
+except Exception as e:
+    logger.exception(e)
+    raise e
